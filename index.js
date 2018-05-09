21e49061bf6b3c90f0865c6d1cc7496d3ee251ed
Tony Shindo <honolulu3310@gmail.com>
2018-05-08 22:22:25 -0400

    Add User authenticaion logic on backend

--- index.js
+++ index.js
@@ -1,9 +1,39 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+const bodyParser = require('body-parser');
+const keys = require('./config/keys');
+
+require('./models/User');
+require('./services/passport');
+
+mongoose.Promise = global.Promise;
+mongoose.connect(keys.mongoURI);
+
 const app = express();
 
-app.get('/test', (req, res) => {
-  res.send({test: "test"});
-});
+app.use(bodyParser.json());
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./routes/authRoutes')(app);
+
+if (['production', 'ci'].includes(process.env.NODE_ENV)) {
+  app.use(express.static('client/build'));
+
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve('client', 'build', 'index.html'));
+  });
+}
 
 
 const PORT = process.env.PORT || 5000;
